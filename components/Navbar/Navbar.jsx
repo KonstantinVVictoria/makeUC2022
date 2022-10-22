647721d8426624f0576b38d22c6871dcf39d6b26
Konstantin Victoria <61480260+KonstantinVVictoria@users.noreply.github.com>
2022-10-22 11:12:50 -0700

    initial commit

---
+++ components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import styles from "./NavBar.module.scss"
+export function NavBar() {
+    return <div>
+        <Title></Title>
+    </div>
+}
+export function Title({children}){
+    return <h1>{chilren}</h1>
+}
+
+export function Icon(){
+    return <div></div>
+}
